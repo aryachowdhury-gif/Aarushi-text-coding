e04b1caff4c38b5f28bcac2d18ad38d1bf9fb2d8
aryachowdhury-gif <arya.chowdhury@gmail.com>
2025-08-24 13:19:41 +0530

    L02

---
+++ L02/L02 HW Storing Birthday.py
@@ -0,0 +1,9 @@
+name1 = print(input("When is your birthday,Aarvi: "))
+name2 = print(input("When is your birthday,Gauri: "))
+name3 = print(input("When is your birthday,Kaashvi: "))
+name4 = print(input("When is your birthday,Tanishka: "))
+
+print("Aarvi your birthday is on ",name1)
+print("Gauri your birthday is on ",name2)
+print("Kaashvi your birthday is on ",name3)
+print("Tanishka your birthday is on ",name4)
